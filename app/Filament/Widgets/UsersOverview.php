2636211a9e363c66506f0b54d5e1f7d29b90aedf
Ihar Karpliuk <karprog01@gmail.com>
2024-07-01 22:17:32 +0300

    bot status in dashboard

--- app/Filament/Widgets/UsersOverview.php
+++ app/Filament/Widgets/UsersOverview.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Widgets;
 
+use App\Models\TelegraphBot;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Override;
@@ -11,22 +12,20 @@ class UsersOverview extends BaseWidget
     #[Override]
     protected function getStats(): array
     {
+        $botStatusValue = '';
+        $botStatusDescription = 'не создан';
+        $botStatusColor = 'danger';
+
+        if ($bot = TelegraphBot::query()->first()) {
+            $botStatusValue = $bot->name;
+            $botStatusDescription = 'активен';
+            $botStatusColor = 'success';
+        }
+
         return [
-            Stat::make('Telegram пользователи', '192.1k')
-                ->description('на 32% больше за 7 дней')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->chart([7, 2, 10, 3, 15, 4, 17])
-                ->color('success'),
-            Stat::make('Оценок за день', '192.1k')
-                ->description('на 32% больше, чем вчера')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->chart([7, 2, 10, 3, 15, 4, 17])
-                ->color('success'),
-            Stat::make('Показов карточек за день', '192.1k')
-                ->description('на 32% больше, чем вчера')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
-                ->chart([7, 2, 10, 3, 15, 4, 17])
-                ->color('success'),
+            Stat::make('Статус Telegram бота', $botStatusValue)
+                ->description($botStatusDescription)
+                ->color($botStatusColor),
         ];
     }
 }
