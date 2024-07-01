297b994a2e608181c09777863c12153b769f980c
Ihar Karpliuk <karprog01@gmail.com>
2024-07-01 22:57:57 +0300

    refactor: remove appending empty middlewares array

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append([
-            //            StartSession::class,
-        ]);
         $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
