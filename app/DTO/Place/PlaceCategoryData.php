06388c85909b58c3aa5674e1a55b13e2e416d73d
Ihar Karpliuk <karprog01@gmail.com>
2024-08-25 11:23:55 +0300

    place category factory from model

--- app/DTO/Place/PlaceCategoryData.php
+++ app/DTO/Place/PlaceCategoryData.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\DTO\Place;
 
+use App\Models\PlaceCategory;
 use Spatie\LaravelData\Data;
 use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 
@@ -15,4 +16,13 @@ class PlaceCategoryData extends Data
         public string $name,
         public string $imageUrl,
     ) {}
+
+    public static function fromPlaceCategory(PlaceCategory $category): self
+    {
+        return self::from([
+            'parent' => $category->parent,
+            'name' => $category->name,
+            'imageUrl' => $category->image->public_path,
+        ]);
+    }
 }
